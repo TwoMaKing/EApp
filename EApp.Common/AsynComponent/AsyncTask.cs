54c325a3b899b3e05428710bb916ab12722357e5
TwoMaKing <airsoft_ft@126.com>
2014-12-08 02:40:34 +0800

    Add base Async task class and sample

---
+++ EApp.Common/AsynComponent/AsyncTask.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EApp.Common.AsynComponent
+{
+    public abstract class AsyncTask : IAsyncTask
+    {
+        private Guid taskId;
+
+        private AsyncOperation asyncOperation;
+
+        private delegate void WorkerEventHandler(AsyncOperation asyncOp, params object[] arguments);
+
+        private HybridDictionary userStateToLifetime = new HybridDictionary();
+
+        private SendOrPostCallback onProgressChangedDelegate;
+
+        private SendOrPostCallback onCompletedDelegate;
+
+        public event ProgressChangedEventHandler ProgressChanged;
+
+        public event AsyncCompletedEventHandler Completed;
+
+        protected AsyncTask()
+        {
+            this.InitializeSendOrPostCallback();
+        }
+
+        public bool CancellationPending
+        {
+            get 
+            {
+                return !this.userStateToLifetime.Contains(taskId);
+            }
+        }
+
+        public void RunAsync(params object[] arguments)
+        {
+            // Create an AsyncOperation for taskId.
+            this.taskId = Guid.NewGuid();
+
+            this.asyncOperation = AsyncOperationManager.CreateOperation(taskId);
+
+            // Multiple threads will access the task dictionary,
+            // so it must be locked to serialize access.
+            lock (this.userStateToLifetime.SyncRoot)
+            {
+                if (this.userStateToLifetime.Contains(taskId))
+                {
+                    throw new ArgumentException("Task ID parameter must be unique", "taskId");
+                }
+
+                this.userStateToLifetime[taskId] = this.asyncOperation;
+            }
+
+            // Start the asynchronous operation.
+            WorkerEventHandler workerDelegate = new WorkerEventHandler(this.DoWork);
+
+            workerDelegate.BeginInvoke(asyncOperation, arguments, null, null);
+        }
+
+        public void CancelAsync()
+        {
+            AsyncOperation asyncOp = this.userStateToLifetime[taskId] as AsyncOperation;
+
+            if (asyncOp != null)
+            {
+                lock (this.userStateToLifetime.SyncRoot)
+                {
+                    this.userStateToLifetime.Remove(taskId);
+                }
+            }
+        }
+
+        public void ReportProgress(int progressPercentage, object userState)
+        {
+            this.ReportProgress(progressPercentage, userState, this.asyncOperation);
+        }
+
+        protected virtual void DoWork(AsyncOperation asyncOp, params object[] arguments) 
+        { 
+            Exception error = null;
+
+            if (!this.CancellationPending)
+            {
+                try
+                {
+                    this.DoWork(arguments);
+                }
+                catch (Exception e)
+                {
+                    error = e;
+                }
+            }
+
+            this.TriggerCompletionEvent(error, this.CancellationPending, this.asyncOperation);
+        }
+
+        #region Private members
+
+        private void InitializeSendOrPostCallback() 
+        {
+            this.onProgressChangedDelegate = new SendOrPostCallback(this.OnTaskProgressChanged);
+            this.onCompletedDelegate = new SendOrPostCallback(this.OnTaskCompleted);
+        }
+
+        private void OnTaskProgressChanged(object state) 
+        {
+            ProgressChangedEventArgs e = state as ProgressChangedEventArgs;
+
+            this.OnTaskProgressChanged(e);
+        }
+
+        private void OnTaskCompleted(object operationState)
+        {
+            AsyncCompletedEventArgs e = operationState as AsyncCompletedEventArgs;
+
+            this.OnTaskCompleted(e);
+        }
+
+        #endregion
+
+        #region Protected members
+
+        protected virtual void OnTaskProgressChanged(ProgressChangedEventArgs e)
+        {
+            if (this.ProgressChanged != null)
+            {
+                this.ProgressChanged(this, e);
+            }
+        }
+
+        protected virtual void OnTaskProgressChanged(int progressPercentage, object userState)
+        {
+            this.OnTaskProgressChanged(new ProgressChangedEventArgs(progressPercentage, userState));
+        }
+
+        protected virtual void OnTaskCompleted(AsyncCompletedEventArgs e)
+        {
+            if (this.Completed != null)
+            {
+                this.Completed(this, e);
+            }
+        }
+
+        protected virtual void OnTaskCompleted(Exception error, bool cancelled, object userState) 
+        {
+            this.OnTaskCompleted(new AsyncCompletedEventArgs(error, cancelled, userState));
+        }
+
+        protected virtual void ReportProgress(int progressPercentage, object userState, AsyncOperation asyncOperation)
+        {
+            Thread.Sleep(1000);
+
+            ProgressChangedEventArgs e = new ProgressChangedEventArgs(progressPercentage, userState);
+
+            asyncOperation.Post(this.onProgressChangedDelegate, e);
+        }
+
+        protected virtual void TriggerCompletionEvent(Exception exception, bool cancelled, AsyncOperation asyncOp)
+        {
+            // If the task was not previously canceled,
+            // remove the task from the lifetime collection.
+            if (!cancelled)
+            {
+                lock (userStateToLifetime.SyncRoot)
+                {
+                    userStateToLifetime.Remove(asyncOp.UserSuppliedState);
+                }
+            }
+
+            AsyncCompletedEventArgs e =
+                new AsyncCompletedEventArgs(
+                exception,
+                cancelled,
+                asyncOp.UserSuppliedState);
+
+            asyncOp.PostOperationCompleted(this.onCompletedDelegate, e);
+        }
+
+        protected abstract void DoWork(params object[] arguments);
+
+        #endregion
+    }
+}
--- Xpress.Mvc/Cost.Designer.cs
+++ Xpress.Mvc/Cost.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.rbMessage = new System.Windows.Forms.RichTextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,12 +41,12 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(780, 355);
+            this.dataGridView1.Size = new System.Drawing.Size(780, 186);
             this.dataGridView1.TabIndex = 0;
             // 
             // btnAddNew
             // 
-            this.btnAddNew.Location = new System.Drawing.Point(12, 382);
+            this.btnAddNew.Location = new System.Drawing.Point(12, 215);
             this.btnAddNew.Name = "btnAddNew";
             this.btnAddNew.Size = new System.Drawing.Size(75, 23);
             this.btnAddNew.TabIndex = 1;
@@ -51,15 +54,46 @@
             this.btnAddNew.UseVisualStyleBackColor = true;
             this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
             // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(12, 268);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(163, 23);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "Export Data To File";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // rbMessage
+            // 
+            this.rbMessage.Location = new System.Drawing.Point(12, 297);
+            this.rbMessage.Name = "rbMessage";
+            this.rbMessage.Size = new System.Drawing.Size(780, 241);
+            this.rbMessage.TabIndex = 3;
+            this.rbMessage.Text = "";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(195, 268);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(143, 23);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel Export";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
             // Cost
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(804, 469);
+            this.ClientSize = new System.Drawing.Size(804, 550);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.rbMessage);
+            this.Controls.Add(this.btnExport);
             this.Controls.Add(this.btnAddNew);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Cost";
-            this.Text = "Form1";
+            this.Text = "MVC Sample";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -69,6 +103,9 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.RichTextBox rbMessage;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
 
